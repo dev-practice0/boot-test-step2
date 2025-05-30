7c8ff7e4ba6220c1d2b096184aa72b3dba112677
qus0in <qus0in@gmail.com>
2025-05-30 11:50:39 +0900

    일단 준비...

---
+++ src/main/java/org/example/bootteststep2/entity/User.java
@@ -0,0 +1,21 @@
+package org.example.bootteststep2.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private int age;
+}
